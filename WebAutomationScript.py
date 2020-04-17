f257f5e3305df87e197a66eb80f1a258d6447bbf
supun64 <l.w.supunmadusanka64@gmail.com>
2020-04-17 13:30:04 +0530

    Basic Command line code to download a video
    This commit for basic command line interface which we can download a video from youtube from a specific file format

--- WebAutomationScript.py
+++
@@ -1,4 +0,0 @@
-from selenium import webdriver
-
-browser = webdriver.Chrome("F:\\Python\\Pycharm\\WebAutomation\\ChromeDriver\\chromedriver.exe")
-browser.get("https://www.youtube.com/watch?v=NnEkVooAsxk&list=PLDDGPdw7e6Ag1EIznZ-m-qXu4XX3A0cIz&index=2")
